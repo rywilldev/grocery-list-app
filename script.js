726b46042c3b3ec05cf4ba2b2c4e8856845c3230
rywilldev <rywilldav@gmail.com>
2023-05-10 14:23:26 -0400

    update style.css
    added cat.png, added styling to input field and add to cart button

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const inputFieldEl = document.querySelector("#input-field");
 const addButtonEl = document.querySelector("#add-button");
 
 addButtonEl.addEventListener("click", function() {
-    inputValue = inputFieldEl.value;
+    let inputValue = inputFieldEl.value;
     console.log(inputValue);
 })
