ce23c3d59388b07ff7aaa94e9801e0f69d4d1dcf
rywilldev <rywilldav@gmail.com>
2023-05-10 14:05:20 -0400

    update input field and add to cart button variable names

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
-let inputEl = document.querySelector("#input-field");
-let inputValue = inputEl.value;
-let inputBtn = document.querySelector("#add-button");
+const inputFieldEl = document.querySelector("#input-field");
+const addButtonEl = document.querySelector("#add-button");
 
-inputBtn.addEventListener("click", function() {
-    inputValue = inputEl.value;
+addButtonEl.addEventListener("click", function() {
+    inputValue = inputFieldEl.value;
     console.log(inputValue);
 })
