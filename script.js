8994ce9dc539439b0d210d33d718ef078519a082
rywilldev <rywilldav@gmail.com>
2023-05-11 08:22:43 -0400

    update script.js
    attached firebase DB to app

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL: "https://realtime-database-b5195-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
 const inputFieldEl = document.querySelector("#input-field");
 const addButtonEl = document.querySelector("#add-button");
+const shoppingListEl = document.querySelector("#shopping-list");
 
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value;
-    console.log(inputValue);
-})
+function addInput() {
+  shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
+}
+
+function clearInput() {
+  inputFieldEl.value = "";
+}
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+  push(shoppingListInDB, inputValue);
+  addInput(inputValue);
+  clearInput();
+});
